15ee9201b1775158319a8556c948d648f6246c91
DongjaJ <jdh981118@naver.com>
2023-10-26 17:58:53 +0900

    chore: lint 종속성 root에 설정

--- animal-friends/packages/eslint-config-custom/index.js
+++ animal-friends/packages/eslint-config-custom/index.js
@@ -7,12 +7,14 @@ module.exports = {
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "prettier",
+    "plugin:react-hooks/recommended",
   ],
   plugins: ["@typescript-eslint"],
   parserOptions: {
     sourceType: "module",
     ecmaVersion: 2020,
   },
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
   rules: {
     "@typescript-eslint/no-non-null-assertion": "off",
   },
