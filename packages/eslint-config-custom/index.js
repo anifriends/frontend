097ca57ff2bdb67559e7ae16a4b97e247f27066f
DongjaJ <jdh981118@naver.com>
2023-11-01 13:13:37 +0900

    chore(common): eslint 설정 변경

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -9,15 +9,9 @@ module.exports = {
     'plugin:prettier/recommended',
     'plugin:react-hooks/recommended',
     'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
   ],
-  plugins: [
-    '@typescript-eslint',
-    'simple-import-sort',
-    'import',
-    'unused-imports',
-    'react-refresh',
-    'react',
-  ],
+  plugins: ['simple-import-sort', 'import', 'unused-imports', 'react-refresh'],
   parserOptions: {
     sourceType: 'module',
     ecmaVersion: 2020,
@@ -51,18 +45,18 @@ module.exports = {
     ],
 
     'no-console': ['warn', { allow: ['warn', 'error'] }], // console.log 금지
-    'react/self-closing-comp': 'warn',
-    curly: 'warn',
-    'react-refresh/only-export-components': ['warn'], // jsx에서 export default 하나만 가능
-    eqeqeq: 'warn', // 일치 연산자 사용 필수
-    'dot-notation': 'warn', // 가능하다면 dot notation 사용
-    'react/destructuring-assignment': 'warn', //state, prop 등에 구조분해 할당 적용
-    'react/jsx-pascal-case': 'warn', // 컴포넌트 이름은 PascalCase로
-    'react/no-direct-mutation-state': 'warn', // state 직접 수정 금지
-    'react/jsx-no-useless-fragment': 'warn', // 불필요한 fragment 금지
-    'react/no-unused-state': 'warn', // 사용되지 않는 state
-    'react/jsx-key': 'warn', // 반복문으로 생성하는 요소에 key 강제
-    'react/jsx-curly-brace-presence': 'warn', // jsx 내 불필요한 중괄호 금지
+    'react/self-closing-comp': 'error',
+    curly: 'error',
+    'react-refresh/only-export-components': ['error'], // jsx에서 export default 하나만 가능
+    eqeqeq: 'error', // 일치 연산자 사용 필수
+    'dot-notation': 'error', // 가능하다면 dot notation 사용
+    'react/destructuring-assignment': 'error', //state, prop 등에 구조분해 할당 적용
+    'react/jsx-pascal-case': 'error', // 컴포넌트 이름은 PascalCase로
+    'react/no-direct-mutation-state': 'error', // state 직접 수정 금지
+    'react/jsx-no-useless-fragment': 'error', // 불필요한 fragment 금지
+    'react/no-unused-state': 'error', // 사용되지 않는 state
+    'react/jsx-key': 'error', // 반복문으로 생성하는 요소에 key 강제
+    'react/jsx-curly-brace-presence': 'error', // jsx 내 불필요한 중괄호 금지
     'react/no-unescaped-entities': 'off', //특수문자 허용
     'react/react-in-jsx-scope': 'off', //import React 검사 끄기
     'react/prop-types': 'off',
