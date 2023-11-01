80d0fa77191cad514c46cbce6df911042d279a26
DongjaJ <jdh981118@naver.com>
2023-11-01 16:05:57 +0900

    feat: tanstack query 및 provider 기초 설정

--- apps/shelter/src/main.tsx
+++ apps/shelter/src/main.tsx
@@ -1,3 +1,5 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
@@ -15,10 +17,15 @@ async function deferRender() {
   return worker.start();
 }
 
+const queryClient = new QueryClient();
+
 deferRender().then(() => {
   ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </React.StrictMode>,
   );
 });
