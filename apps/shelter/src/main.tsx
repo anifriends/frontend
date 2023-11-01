9ff431cfeda2cefac6325116b4261d7eb0387c82
Dongja <jdh981118@naver.com>
2023-11-01 16:19:28 +0900

    chore : msw 의존성 및 설정 추가 및 변경 (#13)
    * chore(common): eslint 설정 변경
    
    * chore(shelter): 보호소 프로젝트에 msw 의존성 추가
    
    * feat: 예시 msw api 추가
    
    * feat: tanstack query 및 provider 기초 설정
    
    * chore(common): eslint 설정 변경

--- apps/shelter/src/main.tsx
+++ apps/shelter/src/main.tsx
@@ -1,10 +1,31 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App.tsx';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+async function deferRender() {
+  if (import.meta.env.MODE !== 'development') {
+    return;
+  }
+
+  const { worker } = await import('./mocks/browser.ts');
+
+  // `worker.start()` returns a Promise that resolves
+  // once the Service Worker is up and ready to intercept requests.
+  return worker.start();
+}
+
+const queryClient = new QueryClient();
+
+deferRender().then(() => {
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </React.StrictMode>,
+  );
+});
