1bb8424b88e446dc2011e390b31923b96114cb58
DongjaJ <jdh981118@naver.com>
2023-11-01 15:59:30 +0900

    feat: 예시 msw api 추가

--- apps/shelter/src/main.tsx
+++ apps/shelter/src/main.tsx
@@ -8,17 +8,17 @@ async function deferRender() {
     return;
   }
 
-  const { worker } = await import('./mocks/browser');
+  const { worker } = await import('./mocks/browser.ts');
 
   // `worker.start()` returns a Promise that resolves
   // once the Service Worker is up and ready to intercept requests.
   return worker.start();
 }
 
-await deferRender();
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+deferRender().then(() => {
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  );
+});
