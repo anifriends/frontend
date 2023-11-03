e228f76cd1bed07aa82ca368a1202fe9bf557a7c
kutta97 <yhjin04170@gmail.com>
2023-11-03 17:04:03 +0900

    feat(ui): bottomNavBar 컴포넌트 추가

---
+++ packages/ui/layout/BottomNavBar/NavBarButton.tsx
@@ -0,0 +1,31 @@
+import { Box, Image } from '@chakra-ui/react';
+
+export type NavBarButtonProps = {
+  onClick: VoidFunction;
+  selected: boolean;
+  buttonImageSrc: [string, string];
+  buttonText: string;
+};
+
+export default function NavBarButton({
+  onClick,
+  selected,
+  buttonImageSrc,
+  buttonText,
+}: NavBarButtonProps) {
+  const [unselectedButton, selectedButton] = buttonImageSrc;
+
+  return (
+    <Box
+      color="gray.500"
+      fontSize="9px"
+      alignItems="center"
+      justifyContent="center"
+      onClick={onClick}
+      as="button"
+    >
+      <Image src={selected ? selectedButton : unselectedButton} />
+      {buttonText}
+    </Box>
+  );
+}
