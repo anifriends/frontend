28a4420eca9bc748a02089c6d845d925c5755e29
kutta97 <yhjin04170@gmail.com>
2023-11-03 17:07:54 +0900

    feat(ui): bottomNavBar 에 네이게이션 버튼 추가

--- apps/volunteer/src/components/layout/index.tsx
+++ apps/volunteer/src/components/layout/index.tsx
@@ -13,7 +13,13 @@ export default function Layout() {
   };
 
   return (
-    <Container maxW="container.sm" h="100vh" p={0} centerContent>
+    <Container
+      position="relative"
+      maxW="container.sm"
+      h="100vh"
+      p={0}
+      centerContent
+    >
       <Header headerOption={headerOption} />
       <Container height="100%" pb="50px" px="16px">
         <Outlet />
--- packages/ui/layout/BottomNavBar/index.tsx
+++ packages/ui/layout/BottomNavBar/index.tsx
@@ -1,17 +1,61 @@
 import { Flex } from '@chakra-ui/react';
 
+import AnimalsSelectedIcon from '../../assets/bottomNavBar/icon_animals_selected.svg';
+import AnimalsUnselectedIcon from '../../assets/bottomNavBar/icon_animals_unselected.svg';
+import ChattingsSelectedIcon from '../../assets/bottomNavBar/icon_chattings_selected.svg';
+import ChattingsUnselectedIcon from '../../assets/bottomNavBar/icon_chattings_unselected.svg';
+import MyPageSeletedIcon from '../../assets/bottomNavBar/icon_mypage_selected.svg';
+import MyPageUnselectedIcon from '../../assets/bottomNavBar/icon_mypage_unselected.svg';
+import VolunteersSelectedIcon from '../../assets/bottomNavBar/icon_volunteers_selected.svg';
+import VolunteersUnselectedIcon from '../../assets/bottomNavBar/icon_volunteers_unselected.svg';
+import NavBarButton from './NavBarButton';
+
 export default function BottomNavBar() {
+  const goVounteers = () => {};
+  const goAnimals = () => {};
+  const goChattings = () => {};
+  const goMyPage = () => {};
+
   return (
     <Flex
+      bgColor="white"
       w="100%"
       h="50px"
+      px={4}
       justify="center"
+      justifyContent="space-between"
+      borderTop="1px solid"
+      borderColor="gray.200"
       align="center"
       bottom={0}
-      pos="fixed"
+      pos="absolute"
       zIndex={10}
+      as="nav"
     >
-      BottomNavBar
+      <NavBarButton
+        selected={false}
+        onClick={goVounteers}
+        buttonImageSrc={[VolunteersUnselectedIcon, VolunteersSelectedIcon]}
+        buttonText="봉사"
+      />
+      <NavBarButton
+        selected={false}
+        onClick={goAnimals}
+        buttonImageSrc={[AnimalsUnselectedIcon, AnimalsSelectedIcon]}
+        buttonText="입양"
+      />
+      <NavBarButton
+        selected={false}
+        onClick={goChattings}
+        buttonImageSrc={[ChattingsUnselectedIcon, ChattingsSelectedIcon]}
+        buttonText="채팅"
+      />
+      <NavBarButton
+        selected={false}
+        onClick={goMyPage}
+        buttonImageSrc={[MyPageUnselectedIcon, MyPageSeletedIcon]}
+        buttonText="마이"
+      />
     </Flex>
   );
 }
