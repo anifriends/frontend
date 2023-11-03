d328a6bd380379ce3eb50e013a93c8f702de6720
Eosdia <lumimomen@gmail.com>
2023-11-03 19:53:42 +0900

    fix(ui): props 변수명 변경, infoTexts 활용

--- packages/ui/components/ProfileInfo.tsx
+++ packages/ui/components/ProfileInfo.tsx
@@ -1,38 +1,34 @@
 import { Avatar, HStack, Text, VStack } from '@chakra-ui/react';
 
 type ProfileInfoProps = {
-  imageUrl?: string;
-  name: string;
-  email: string;
-  address?: string;
-  phoneNumber?: string;
+  infoImage?: string;
+  infoTitle: string;
+  infoTexts?: string[];
   children?: React.ReactNode;
 };
 
 export default function ProfileInfo({
-  imageUrl,
-  name,
-  email,
-  address,
-  phoneNumber,
+  infoImage,
+  infoTitle,
+  infoTexts,
   children,
 }: ProfileInfoProps) {
   return (
     <HStack spacing={5} py={3.5} align="flex-start">
-      <Avatar src={imageUrl} boxSize="4.4rem" />
+      <Avatar src={infoImage} boxSize="4.4rem" />
       <VStack align="left" gap={0}>
         <HStack align="center" spacing={3}>
           <Text fontSize="md" fontWeight="semibold">
-            {name}
+            {infoTitle}
           </Text>
           {children}
         </HStack>
-        <Text fontSize="sm" fontWeight="normal">
-          {email}
-        </Text>
-        <Text fontSize="sm" fontWeight="normal">
-          {address || phoneNumber}
-        </Text>
+        {infoTexts &&
+          infoTexts.map((infoText, index) => (
+            <Text key={index} fontSize="sm" fontWeight="normal">
+              {infoText}
+            </Text>
+          ))}
       </VStack>
     </HStack>
   );
