ed39096343c6bcc688a5c8017cb714a43a30f21c
sukvvon <sukvvon@gmail.com>
2023-11-03 17:40:24 +0900

    feat(ui): InfoTextItem 컴포넌트 추가

---
+++ packages/ui/components/InfoTextItem.tsx
@@ -0,0 +1,46 @@
+import type { ColorProps } from '@chakra-ui/react';
+import { Flex, Text } from '@chakra-ui/react';
+
+type InfoTextItemStyles = {
+  titleColor: ColorProps['color'];
+  contentColor: ColorProps['color'];
+};
+
+export type InfoTextItemPropsWithoutStyles = {
+  title: string;
+  content: string;
+  isList?: boolean;
+};
+
+type InfoTextItemProps = InfoTextItemStyles & InfoTextItemPropsWithoutStyles;
+
+export default function InfoTextItem({
+  title,
+  content,
+  titleColor,
+  contentColor,
+  isList = false,
+}: InfoTextItemProps) {
+  return (
+    <Flex minH={5} gap={isList ? '5%' : 1}>
+      <Text
+        w={isList ? '25%' : undefined}
+        color={titleColor}
+        size="md"
+        fontWeight={isList ? 'normal' : 'medium'}
+        fontSize={isList ? 'sm' : 'xs'}
+      >
+        {title}
+      </Text>
+      <Text
+        w={isList ? '70%' : undefined}
+        color={contentColor}
+        size="md"
+        fontWeight={isList ? 'medium' : 'semibold'}
+        fontSize={isList ? 'sm' : 'xs'}
+      >
+        {content}
+      </Text>
+    </Flex>
+  );
+}
