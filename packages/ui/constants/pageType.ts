44fb16249cbbfc91e45549701f03ea424b5c47e8
kutta97 <yhjin04170@gmail.com>
2023-11-03 15:01:19 +0900

    refactor(ui): pageType 의 HEADER_TYPE 내용을 구조분해 할당으로 꺼내 사용하도록 수정

--- packages/ui/constants/pageType.ts
+++ packages/ui/constants/pageType.ts
@@ -1,105 +1,107 @@
 import HEADER_TYPE from './headerType';
 
+const { DEFAULT, DETAIL, SEARCH } = HEADER_TYPE;
+
 const PAGE_TYPE = {
   VOLUNTEERS: {
     title: '봉사자 모집',
-    headerType: HEADER_TYPE.DEFAULT,
+    headerType: DEFAULT,
   },
   VOLUNTEERS_DETAIL: {
     title: '봉사자 모집 상세',
-    headerType: HEADER_TYPE.DETAIL,
+    headerType: DETAIL,
   },
   VOLUNTEERS_PROFILE: {
     title: '봉사자 프로필',
-    headerType: HEADER_TYPE.DETAIL,
+    headerType: DETAIL,
   },
   VOLUNTEERS_SEARCH: {
     title: '봉사자 모집글 검색',
-    headerType: HEADER_TYPE.SEARCH,
+    headerType: SEARCH,
   },
   VOLUNTEERS_WRITE: {
     title: '봉사자 모집글 작성',
-    headerType: HEADER_TYPE.DETAIL,
+    headerType: DETAIL,
   },
   VOLUNTEERS_UPDATE: {
     title: '봉사자 모집글 수정',
-    headerType: HEADER_TYPE.DETAIL,
+    headerType: DETAIL,
   },
   ANIMALS: {
     title: '유기보호 동물',
-    headerType: HEADER_TYPE.DEFAULT,
+    headerType: DEFAULT,
   },
   ANIMALS_DETAIL: {
     title: '유기보호 동물 상세',
-    headerType: HEADER_TYPE.DEFAULT,
+    headerType: DEFAULT,
   },
   ANIMALS_SEARCH: {
     title: '유기보호 동물 검색',
-    headerType: HEADER_TYPE.SEARCH,
+    headerType: SEARCH,
   },
   ANIMALS_WRITE: {
     title: '유기보호 동물 작성',
-    headerType: HEADER_TYPE.DETAIL,
+    headerType: DETAIL,
   },
   ANIMALS_UPDATE: {
     title: '유기보호 동물 수정',
-    headerType: HEADER_TYPE.DETAIL,
+    headerType: DETAIL,
   },
   CHATTINGS: {
     title: '채팅',
-    headerType: HEADER_TYPE.DEFAULT,
+    headerType: DEFAULT,
   },
   CHATTINGS_ROOM: {
     title: '채팅방',
-    headerType: HEADER_TYPE.DETAIL,
+    headerType: DETAIL,
   },
   MYPAGE: {
     title: '마이페이지',
-    headerType: HEADER_TYPE.DEFAULT,
+    headerType: DEFAULT,
   },
   MYPAGE_REVIEWS: {
     title: '봉사 후기',
-    headerType: HEADER_TYPE.DETAIL,
+    headerType: DETAIL,
   },
   SETTINGS_ACCOUNT: {
     title: '계정 정보 수정',
-    headerType: HEADER_TYPE.DETAIL,
+    headerType: DETAIL,
   },
   SETTINGS_PASSWORD: {
     title: '비밀 번호 수정',
-    headerType: HEADER_TYPE.DETAIL,
+    headerType: DETAIL,
   },
   MANAGE_ATTENDANCE: {
     title: '봉사자 출석 관리',
-    headerType: HEADER_TYPE.DETAIL,
+    headerType: DETAIL,
   },
   MANAGE_APPLY: {
     title: '봉사자 신청 현황',
-    headerType: HEADER_TYPE.DETAIL,
+    headerType: DETAIL,
   },
   NOTIFICATIONS: {
     title: '알림',
-    headerType: HEADER_TYPE.DETAIL,
+    headerType: DETAIL,
   },
   SHELTERS_PROFILE: {
     title: '보호소 프로필',
-    headerType: HEADER_TYPE.DETAIL,
+    headerType: DETAIL,
   },
   SHELTERS_REVIEWS_WRITE: {
     title: '봉사 후기 작성',
-    headerType: HEADER_TYPE.DETAIL,
+    headerType: DETAIL,
   },
   SHELTERS_REVIEWS_UPDATE: {
     title: '봉사 후기 수정',
-    headerType: HEADER_TYPE.DETAIL,
+    headerType: DETAIL,
   },
   SIGNUP: {
     title: '회원가입',
-    headerType: HEADER_TYPE.DEFAULT,
+    headerType: DEFAULT,
   },
   SIGNIN: {
     title: '로그인',
-    headerType: HEADER_TYPE.DEFAULT,
+    headerType: DEFAULT,
   },
 } as const;
 
