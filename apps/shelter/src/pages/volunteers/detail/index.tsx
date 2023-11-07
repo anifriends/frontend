44e7567bb68a1b0b1fced567e105f1f625c2908a
kutta97 <yhjin04170@gmail.com>
2023-11-08 05:15:47 +0900

    feat(shared): detailHeaderStore 와 DetailPage 연결

--- apps/shelter/src/pages/volunteers/detail/index.tsx
+++ apps/shelter/src/pages/volunteers/detail/index.tsx
@@ -1,3 +1,21 @@
+import { useEffect } from 'react';
+import useDetailHeaderStore from 'shared/store/detailHeaderStore';
+
+const handleDeletePost = (postId: number) => {
+  // TODO: VolunteerPost delete API 호출
+  console.log('[Delete Volunteer] postId:', postId);
+};
+
 export default function VolunteersDetailPage() {
+  const setOnDelete = useDetailHeaderStore((state) => state.setOnDelete);
+
+  useEffect(() => {
+    setOnDelete(handleDeletePost);
+
+    return () => {
+      setOnDelete(() => {});
+    };
+  }, [setOnDelete]);
+
   return <h1>VolunteersDetailPage</h1>;
 }
