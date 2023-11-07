5c82273ac374581f5a59b5ae44dfa2a4e70af13f
DongjaJ <jdh981118@naver.com>
2023-11-07 11:55:23 +0900

    feat(shared): reviewItem 컴포넌트

---
+++ packages/shared/components/ReviewItem.tsx
@@ -0,0 +1,49 @@
+import { Button, Card, HStack, Image, Text } from '@chakra-ui/react';
+import React from 'react';
+
+import SettingIcon from '../assets/icon_menu.svg';
+
+export default function ReviewItem({
+  children,
+  content,
+  images,
+}: {
+  children: React.ReactNode;
+  content: string;
+  images: string[];
+}) {
+  return (
+    <Card p={4} w="342px" h="273px" gap={3.5}>
+      {children}
+      <Button
+        minW="1.25rem"
+        w="1.25rem"
+        h="1.25rem"
+        p={0}
+        bgColor="transparent"
+        color="gray.500"
+        pos="absolute"
+        top={4}
+        right={4}
+      >
+        <Image src={SettingIcon} alt="setting icon" w="full" h="full" />
+      </Button>
+      <Text fontSize="xs" lineHeight="4">
+        {content}
+      </Text>
+      <HStack
+        spacing="0.375rem"
+        overflowX="scroll"
+        sx={{
+          '::-webkit-scrollbar': {
+            display: 'none',
+          },
+        }}
+      >
+        {images.map((src, index) => (
+          <Image w="8.75rem" h="8.75rem" src={src} key={index} flexShrink={0} />
+        ))}
+      </HStack>
+    </Card>
+  );
+}
