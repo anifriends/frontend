e743c6602a86c841f88d42062ccec12cdef7f128
kutta97 <yhjin04170@gmail.com>
2023-11-08 05:15:19 +0900

    feat(shared): searchHeaderStore 와 SearchPage 연결

--- apps/volunteer/src/pages/volunteers/search/index.tsx
+++ apps/volunteer/src/pages/volunteers/search/index.tsx
@@ -1,3 +1,21 @@
+import { useEffect } from 'react';
+import useSearchHeaderStore from 'shared/store/searchHeaderStore';
+
+const handleSearchkeyword = (keyword: string) => {
+  // TODO: VolunteerList 검색 API 호출
+  console.log('[Search Volunteer] keyword:', keyword);
+};
+
 export default function VolunteersSearchPage() {
+  const setOnSearch = useSearchHeaderStore((state) => state.setOnSearch);
+
+  useEffect(() => {
+    setOnSearch(handleSearchkeyword);
+
+    return () => {
+      setOnSearch(() => {});
+    };
+  }, [setOnSearch]);
+
   return <h1>VolunteersSearchPage</h1>;
 }
