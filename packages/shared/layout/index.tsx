267cc36fea61dc206cc4892ecefbc6ad4b757ccc
kutta97 <yhjin04170@gmail.com>
2023-11-07 14:09:23 +0900

    refactor(shared): layout 컴포넌트 컨텐츠를 감싸는 컴포넌트를 Box 로 변경

--- packages/shared/layout/index.tsx
+++ packages/shared/layout/index.tsx
@@ -1,4 +1,4 @@
-import { Container } from '@chakra-ui/react';
+import { Box, Container } from '@chakra-ui/react';
 import { Outlet } from 'react-router-dom';
 
 import BottomNavBar from './BottomNavBar';
@@ -8,9 +8,9 @@ export default function Layout() {
   return (
     <Container pos="relative" maxW="container.sm" h="100vh" p={0} centerContent>
       <Header />
-      <Container maxW="inherit" height="100%" pb="50px" px={0} as="main">
+      <Box width="100%" height="100%" pb="50px" as="main">
         <Outlet />
-      </Container>
+      </Box>
       <BottomNavBar />
     </Container>
   );
