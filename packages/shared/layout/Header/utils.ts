92d6a96214b17d1a5f559b1d1c2ede354fe6fc70
kutta97 <yhjin04170@gmail.com>
2023-11-07 21:30:54 +0900

    refactor(shared): getHeaderType 및 getHeaderTitle 함수 로직 변경

--- packages/shared/layout/Header/utils.ts
+++ packages/shared/layout/Header/utils.ts
@@ -1,8 +1,24 @@
-import PAGE_TYPE from '../../constants/pageType';
+import headerTitle from '../../constants/headerTitle';
+import HEADER_TYPE from '../../constants/headerType';
 import { HeaderType } from '../../types/header';
 import { PageType } from '../../types/page';
 
-export const getHeaderType = (pageType: PageType): HeaderType =>
-  PAGE_TYPE[pageType].headerType;
+export const getHeaderType = (pageType: PageType): HeaderType => {
+  if (
+    pageType === 'VOLUNTEERS' ||
+    pageType === 'ANIMALS' ||
+    pageType === 'CHATTINGS' ||
+    pageType === 'MYPAGE'
+  ) {
+    return HEADER_TYPE.DEFAULT;
+  }
 
-export const getHeaderTitle = (pageType: PageType) => PAGE_TYPE[pageType].title;
+  if (pageType === 'VOLUNTEERS_SEARCH' || pageType === 'ANIMALS_SEARCH') {
+    return HEADER_TYPE.SEARCH;
+  }
+
+  return HEADER_TYPE.DETAIL;
+};
+
+export const getHeaderTitle = (pageType: PageType): string =>
+  headerTitle[pageType];
