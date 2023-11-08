e8daaa8848aa3179f00cb91d47b843a1e0b8fb50
kutta97 <yhjin04170@gmail.com>
2023-11-08 16:50:40 +0900

    feat(shared): content 위치에 컴포넌트를 받을 수 있는 InfoItem 컴포넌트 개발

---
+++ packages/shared/components/InfoItem.tsx
@@ -0,0 +1,38 @@
+import type { FlexProps, TextProps } from '@chakra-ui/react';
+import { Flex, Text } from '@chakra-ui/react';
+import { ReactElement } from 'react';
+
+export type InfoItemStylesProps = {
+  flexStyles: FlexProps;
+  titleTextStyles: TextProps;
+};
+
+type InfoItemWithoutStylesProps = {
+  title: string;
+  children: ReactElement;
+};
+
+export type InfoItemProps = Partial<InfoItemStylesProps> &
+  InfoItemWithoutStylesProps;
+
+export default function InfoItem({
+  title,
+  children,
+  flexStyles,
+  titleTextStyles,
+}: InfoItemProps) {
+  return (
+    <Flex gap={1} {...flexStyles}>
+      <Text
+        color="gray.400"
+        size="md"
+        fontWeight="medium"
+        fontSize="xs"
+        {...titleTextStyles}
+      >
+        {title}
+      </Text>
+      {children}
+    </Flex>
+  );
+}
