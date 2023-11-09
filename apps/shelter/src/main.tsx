ccc9c9fbe5b8b534e4c3a46b29288ca2761cf5ef
sukvvon <sukvvon@gmail.com>
2023-11-10 01:59:03 +0900

    feat(shelter): Fonts 컴포넌트 App에 추가 및 Provider 위치 통일

--- apps/shelter/src/main.tsx
+++ apps/shelter/src/main.tsx
@@ -1,5 +1,3 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
@@ -17,15 +15,10 @@ async function deferRender() {
   return worker.start();
 }
 
-const queryClient = new QueryClient();
-
 deferRender().then(() => {
   ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-      <QueryClientProvider client={queryClient}>
-        <App />
-        <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
+      <App />
     </React.StrictMode>,
   );
 });
