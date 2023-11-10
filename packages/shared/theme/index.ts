2cceea6e9b477bbe2385bc97ce2c695a7624cd22
sukvvon <sukvvon@gmail.com>
2023-11-10 14:50:31 +0900

    feat(shared): fonts를 포함한 theme 추가

---
+++ packages/shared/theme/index.ts
@@ -0,0 +1,10 @@
+import { extendTheme } from '@chakra-ui/react';
+
+const theme = extendTheme({
+  fonts: {
+    heading: `'Pretendard-Heading'`,
+    body: `'Pretendard-Body'`,
+  },
+});
+
+export default theme;
