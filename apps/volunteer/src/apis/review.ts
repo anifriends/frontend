d1f31b061f01408743bc080f2bb838ef17a086ce
DongjaJ <jdh981118@naver.com>
2023-11-13 14:53:37 +0900

    refactor(shelter): shelter review api 이동

---
+++ apps/volunteer/src/apis/review.ts
@@ -0,0 +1,28 @@
+import axiosInstance from 'shared/apis/axiosInstance';
+
+export const getShelterReviewList = (
+  shelterId: number,
+  pageNumber: number,
+  pageSize: number,
+) =>
+  axiosInstance.get<{
+    pageInfo: {
+      totalElements: 32;
+      hasNext: boolean;
+    };
+    reviews: {
+      reviewId: number;
+      reviewCreatedAt: string;
+      reviewContent: string;
+      reviewImageUrls: string[];
+      volunteerName: string;
+      volunteerTemperature: number;
+      volunteerReviewCount: number;
+      volunteerImageUrl: number;
+    }[];
+  }>(`/shelters/${shelterId}/reviews`, {
+    params: {
+      pageSize,
+      pageNumber,
+    },
+  });
