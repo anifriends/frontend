c65a505fbb74e1eb57103375fe6d0f196b8c1f30
kutta97 <yhjin04170@gmail.com>
2023-11-15 03:49:53 +0900

    feat(shelter): useVolunteerSearch() hook 내부에 useFetchVolunteers 로직 추가

--- apps/shelter/src/pages/volunteers/search/_hooks/useVolunteerSearch.ts
+++ apps/shelter/src/pages/volunteers/search/_hooks/useVolunteerSearch.ts
@@ -1,36 +1,42 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import useSearchHeaderStore from 'shared/store/searchHeaderStore';
 
+import { RecruitSearchFilter } from '@/apis/recruitment';
+import useFetchVolunteers from '@/pages/volunteers/hooks/useFetchVolunteers';
+import useIntersect from '@/pages/volunteers/hooks/useIntersection';
+import { useSearchFilter } from '@/pages/volunteers/search/_hooks/useSearchFilter';
 import { useVolunteerSearchFilter } from '@/pages/volunteers/search/_hooks/useVolunteerSearchFilter';
 
-const DUMMY_RECRUITMENT = {
-  recruitmentId: 1,
-  recruitmentTitle: '봉사자를 모집합니다',
-  recruitmentStartTime: '2021-11-08T11:44:30.327959',
-  recruitmentEndTime: '2021-11-08T11:44:30.327959',
-  recruitmentDeadline: '2021-11-08T11:44:30.327959',
-  recruitmentIsClosed: false,
-  recruitmentApplicantCount: 15,
-  recruitmentCapacity: 15,
-};
+const PAGE_SIZE = 10;
 
 export const useVolunteerSearch = () => {
-  const [recruitmentList, setRecruitmentList] = useState<unknown[]>([]);
+  const [searchFilter, setSearchFilter] =
+    useSearchFilter<RecruitSearchFilter>();
 
-  useEffect(() => {
-    setRecruitmentList(Array.from({ length: 10 }, () => DUMMY_RECRUITMENT));
-  }, []);
-
-  const searchAPI = async () => {
-    // TODO: API call
-  };
+  const { volunteerSearchFilter, setVolunteerSearchFilter } =
+    useVolunteerSearchFilter(searchFilter, setSearchFilter);
 
   const {
-    isSearched,
-    setKeywordFilter,
-    volunteerSearchFilter,
-    setVolunteerSearchFilter,
-  } = useVolunteerSearchFilter(searchAPI);
+    data: { pages },
+    hasNextPage,
+    isFetchingNextPage,
+    fetchNextPage,
+  } = useFetchVolunteers(PAGE_SIZE, searchFilter);
+
+  const ref = useIntersect(async (entry, observer) => {
+    observer.unobserve(entry.target);
+    if (hasNextPage && !isFetchingNextPage) {
+      fetchNextPage();
+    }
+  });
+
+  useEffect(() => {
+    if (Object.keys(searchFilter).length === 0) {
+      return;
+    }
+
+    fetchNextPage();
+  }, [searchFilter]);
 
   const [setOnSearch, setKeyword] = useSearchHeaderStore((state) => [
     state.setOnSearch,
@@ -38,7 +44,7 @@ export const useVolunteerSearch = () => {
   ]);
 
   useEffect(() => {
-    setOnSearch(setKeywordFilter);
+    setOnSearch((keyword) => setSearchFilter({ keyword }));
 
     return () => {
       setKeyword('');
@@ -47,8 +53,9 @@ export const useVolunteerSearch = () => {
   }, []);
 
   return {
-    isSearched,
-    recruitmentList,
+    ref,
+    isSearched: Boolean(searchFilter.keyword),
+    recruitmentList: pages.flatMap(({ recruitments }) => recruitments),
     volunteerSearchFilter,
     setVolunteerSearchFilter,
   };
