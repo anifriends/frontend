f5277c045b6430505d83503644bef88c08d25e76
kutta97 <yhjin04170@gmail.com>
2023-11-14 13:28:28 +0900

    feat(shelter): msw 에 shelterHandlers 추가

--- apps/shelter/src/mocks/browser.ts
+++ apps/shelter/src/mocks/browser.ts
@@ -1,5 +1,6 @@
 import { setupWorker } from 'msw/browser';
 
-import { handlers } from './handlers/auth';
+import { handlers as authHandlers } from './handlers/auth';
+import { handlers as shelterHandlers } from './handlers/shelter';
 
-export const worker = setupWorker(...handlers);
+export const worker = setupWorker(...authHandlers, ...shelterHandlers);
