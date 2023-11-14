cb9c462bffa8cc129622a185cf416c2e5567d7f5
kutta97 <yhjin04170@gmail.com>
2023-11-15 01:53:51 +0900

    fix(shelter): useFetchVolunteers return value의 type infer 제대로 되지 않는 버그 수정

--- apps/shelter/src/pages/volunteers/hooks/useFetchVolunteers.tsx
+++ apps/shelter/src/pages/volunteers/hooks/useFetchVolunteers.tsx
@@ -1,14 +1,26 @@
-import { useSuspenseInfiniteQuery } from '@tanstack/react-query';
+import {
+  InfiniteData,
+  useSuspenseInfiniteQuery,
+  UseSuspenseInfiniteQueryResult,
+} from '@tanstack/react-query';
 
-import { getShelterRecruitments } from '@/apis/recruitment';
+import {
+  getShelterRecruitments,
+  RecruitmentSearchResponse,
+} from '@/apis/recruitment';
 
-export default function useFetchVolunteers(pageSize: number) {
+export default function useFetchVolunteers(
+  pageSize: number,
+): UseSuspenseInfiniteQueryResult<
+  InfiniteData<RecruitmentSearchResponse>,
+  Error
+> {
   return useSuspenseInfiniteQuery({
     queryKey: ['recruitments'],
     queryFn: ({ pageParam }) =>
       getShelterRecruitments({ pageNumber: pageParam, pageSize }),
     initialPageParam: 0,
-    getNextPageParam: ({ data: { pageInfo } }, _, lastPageParam) =>
+    getNextPageParam: ({ pageInfo }, _, lastPageParam) =>
       pageInfo.hasNext ? lastPageParam + 1 : null,
   });
 }
