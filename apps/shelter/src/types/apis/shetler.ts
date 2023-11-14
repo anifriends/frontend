087122f541270449e64cf96d95c148e39628ffe7
kutta97 <yhjin04170@gmail.com>
2023-11-14 13:25:55 +0900

    refactor(shelter): getShelterInfo API 리팩토링

---
+++ apps/shelter/src/types/apis/shetler.ts
@@ -0,0 +1,11 @@
+export type ShelterInfo = {
+  shelterId: number;
+  shelterEmail: string;
+  shelterName: string;
+  shelterImageUrl: string;
+  shelterAddress: string;
+  shelterAddressDetail: string;
+  shelterPhoneNumber: string;
+  shelterSparePhoneNumber: string;
+  shelterIsOpenedAddress: boolean;
+};
