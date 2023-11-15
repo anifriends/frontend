4d01a8edc616f643aa47a79efd1cc1ace69f418e
sukvvon <sukvvon@gmail.com>
2023-11-15 17:08:41 +0900

    feat(volunteer): auth(signin, signup) mock handler 추가

--- apps/volunteer/src/mocks/handlers/auth.ts
+++ apps/volunteer/src/mocks/handlers/auth.ts
@@ -7,9 +7,43 @@ export const handlers = [
       {
         accessToken: 'accessToken',
         userId: 1,
-        role: 'role',
+        role: 'ROLE_VOLUNTEER',
       },
       { status: 200 },
+    );
+  }),
+  http.post('/auth/volunteers/login', async () => {
+    await delay(200);
+    return HttpResponse.json(
+      {
+        errorCode: 'AF002',
+        message: '이메일/비밀번호가 올바르지 않습니다',
+      },
+      { status: 400 },
+    );
+  }),
+  http.post('/auth/volunteers/login', async () => {
+    await delay(200);
+    return HttpResponse.json(
+      {
+        errorCode: 'AF001',
+        message: '잘못된 입력값입니다',
+      },
+      { status: 400 },
+    );
+  }),
+  http.post('/volunteers', async () => {
+    await delay(200);
+    return HttpResponse.json({}, { status: 200 });
+  }),
+  http.post('/volunteers', async () => {
+    await delay(200);
+    return HttpResponse.json(
+      {
+        errorCode: 'AF001',
+        message: '{입력값}이 잘못되었습니다.',
+      },
+      { status: 400 },
     );
   }),
   http.post('/volunteers/email', async () => {
@@ -19,6 +53,16 @@ export const handlers = [
         isDuplicated: false,
       },
       { status: 200 },
+    );
+  }),
+  http.post('/volunteers/email', async () => {
+    await delay(200);
+    return HttpResponse.json(
+      {
+        errorCode: 'AF001',
+        message: '잘못된 입력값입니다',
+      },
+      { status: 400 },
     );
   }),
 ];
