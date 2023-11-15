f23fc724f0354b67e60bbc16ce6425c81b880c2d
sukvvon <sukvvon@gmail.com>
2023-11-15 17:09:41 +0900

    feat(shelter): auth(signin, signup) mock handler 추가

--- apps/shelter/src/mocks/handlers/auth.ts
+++ apps/shelter/src/mocks/handlers/auth.ts
@@ -7,9 +7,53 @@ export const handlers = [
       {
         accessToken: 'accessToken',
         userId: 1,
-        role: 'shelter',
+        role: 'ROLE_SEHLTER',
       },
       { status: 200 },
+    );
+  }),
+  http.post('/auth/shelters/login', async () => {
+    await delay(200);
+    return HttpResponse.json(
+      {
+        errorCode: 'AF002',
+        message: '이메일/비밀번호가 올바르지 않습니다',
+      },
+      { status: 400 },
+    );
+  }),
+  http.post('/auth/shelters/login', async () => {
+    await delay(200);
+    return HttpResponse.json(
+      {
+        errorCode: 'AF001',
+        message: '잘못된 입력값입니다',
+      },
+      { status: 400 },
+    );
+  }),
+  http.post('/shelters', async () => {
+    await delay(200);
+    return HttpResponse.json({}, { status: 400 });
+  }),
+  http.post('/shelters', async () => {
+    await delay(200);
+    return HttpResponse.json(
+      {
+        errorCode: 'AF002',
+        message: '{입력값}은 1자 이상, 20자 이하여야 합니다.',
+      },
+      { status: 400 },
+    );
+  }),
+  http.post('/shelters', async () => {
+    await delay(200);
+    return HttpResponse.json(
+      {
+        errorCode: 'AF001',
+        message: '요청값이 입력되지 않았습니다.. {}',
+      },
+      { status: 400 },
     );
   }),
   http.post('/shelters/email', async () => {
@@ -19,6 +63,16 @@ export const handlers = [
         isDuplicated: false,
       },
       { status: 200 },
+    );
+  }),
+  http.post('/shelters/email', async () => {
+    await delay(200);
+    return HttpResponse.json(
+      {
+        errorCode: 'AF001',
+        message: '잘못된 입력값입니다',
+      },
+      { status: 400 },
     );
   }),
 ];
