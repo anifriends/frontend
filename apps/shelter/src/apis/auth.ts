a5f88a27f60800a4902d62b969f28a224876ba3c
sukvvon <sukvvon@gmail.com>
2023-11-15 17:32:51 +0900

    feat(shelter): auth(signin, signup) 관련 api 함수 추가

--- apps/shelter/src/apis/auth.ts
+++ apps/shelter/src/apis/auth.ts
@@ -1,46 +1,68 @@
+import type { AxiosResponse } from 'axios';
+import { AxiosError } from 'axios';
 import axiosInstance from 'shared/apis/axiosInstance';
+import type {
+  checkDuplicatedEmailParams,
+  checkDuplicatedEmailResponse,
+  SigninParams,
+  SigninResponse,
+} from 'shared/types/apis/auth';
+import type { ErrorResponse } from 'shared/types/apis/error';
 
-type SignUpParams = {
-  email: string;
-  password: string;
-  name: string;
-  address: string;
-  addressDetail: string;
-  phoneNumber: string;
-  sparePhoneNumber: string;
-  isOpenedAddress: boolean;
+import { SignupParams } from '@/types/apis/auth';
+
+export const signinShelter = async (
+  data: SigninParams,
+): Promise<AxiosResponse<SigninResponse> | AxiosError<ErrorResponse>> => {
+  try {
+    const response = await axiosInstance.post<SigninResponse, SigninParams>(
+      '/auth/shelters/login',
+      data,
+    );
+
+    return response;
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      return error;
+    }
+
+    throw error;
+  }
 };
 
-export const signUpShelter = (signUpParams: SignUpParams) =>
-  axiosInstance.post<unknown, SignUpParams>('/shelters', signUpParams);
-
-export const signInShelter = ({
-  email,
-  password,
-}: {
-  email: string;
-  password: string;
-}) =>
-  axiosInstance.post<
-    {
-      accessToken: string;
-      userId: number;
-      role: string;
-    },
-    {
-      email: string;
-      password: string;
+export const signupShelter = async (
+  data: SignupParams,
+): Promise<AxiosError<ErrorResponse> | void> => {
+  try {
+    await axiosInstance.post<unknown, SignupParams>('/shelters', data);
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      return error;
     }
-  >('/auth/shelters/login', {
-    email,
-    password,
-  });
 
-export const checkDuplicatedShelterEmail = (email: string) => {
-  return axiosInstance.post<{ isDuplicated: boolean }, { email: string }>(
-    '/shelters/email',
-    {
+    throw error;
+  }
+};
+
+export const checkDuplicatedShelterEmail = async (
+  email: string,
+): Promise<
+  AxiosResponse<checkDuplicatedEmailResponse> | AxiosError<ErrorResponse>
+> => {
+  try {
+    const response = axiosInstance.post<
+      checkDuplicatedEmailResponse,
+      checkDuplicatedEmailParams
+    >('/shelters/email', {
       email,
-    },
-  );
+    });
+
+    return response;
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      return error;
+    }
+
+    throw error;
+  }
 };
