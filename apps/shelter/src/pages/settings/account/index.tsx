01892c0f86430ec5349f5ad64e5afe5e20e39768
woo hyeonji <117665863+Eosdia@users.noreply.github.com>
2023-11-20 17:24:56 +0900

    feat: 계정 수정 페이지 UI (봉사자, 보호소) (#131)
    * feat(shared): input 의 icon_phone svg 추가
    
    * feat(shelter): 계정정보수정페이지 UI 개발
    
    * fix(volunteer): setting 페이지 route 버그 수정
    
    * feat(volunteer): 계정정보 수정 페이지  UI
    
    * design(volunteer): 전화번호 placeholder 추가
    
    * design(shelter): formLabel, formControl 여백 수정
    
    * design(volunteer): formLabel, formControl 여백 수정
    
    * design(shelter): 전화번호 icon 삭제
    
    * design(volunteer): 전화번호 icon 삭제
    
    * remove(shared): icon_phone svg 삭제

--- apps/shelter/src/pages/settings/account/index.tsx
+++ apps/shelter/src/pages/settings/account/index.tsx
@@ -1,3 +1,112 @@
+import {
+  Avatar,
+  Box,
+  Button,
+  Center,
+  FormControl,
+  FormLabel,
+  HStack,
+  Input,
+  Switch,
+} from '@chakra-ui/react';
+import { useState } from 'react';
+
 export default function SettingsAccountPage() {
-  return <h1>SettingsAccountPage</h1>;
+  const [imgFile, setImgFile] = useState<string>('');
+
+  const uploadImgFile = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      const newImgFile = URL.createObjectURL(file);
+      setImgFile(newImgFile);
+    }
+  };
+
+  return (
+    <Box px={4} pb={193}>
+      <form>
+        <Center py={30}>
+          <FormLabel htmlFor="fileInput" cursor="pointer" fontWeight={400} />
+          <Avatar
+            boxSize={100}
+            as="label"
+            htmlFor="profileImg"
+            cursor="pointer"
+            src={imgFile}
+          />
+          <Input
+            id="profileImg"
+            type="file"
+            accept="image/*"
+            display="none"
+            onChange={uploadImgFile}
+          />
+        </Center>
+
+        <FormControl mb={5}>
+          <FormLabel fontWeight={400}>이메일</FormLabel>
+          <Input
+            disabled
+            bgColor="gray.100"
+            color="gray.500"
+            _hover={{ border: 'none' }}
+          />
+        </FormControl>
+
+        <FormControl mb={5} isRequired>
+          <FormLabel fontWeight={400}>보호소 이름</FormLabel>
+          <Input placeholder="이름을 입력하세요" />
+        </FormControl>
+
+        <FormControl mb={5} isRequired>
+          <FormLabel fontWeight={400}>보호소 주소</FormLabel>
+          <Input placeholder="보호소 주소를 입력해주세요" />
+        </FormControl>
+
+        <FormControl mb={5} isRequired>
+          <HStack mb={2}>
+            <FormLabel m={0} fontWeight={400} w="100%">
+              보호소 상세 주소
+            </FormLabel>
+            <FormControl as={HStack} justify="flex-end" spacing={1}>
+              <FormLabel
+                m={0}
+                fontSize="sm"
+                color="gray.500"
+                pos="relative"
+                top="1px"
+              >
+                상세주소 공개
+              </FormLabel>
+              <Switch colorScheme="orange" />
+            </FormControl>
+          </HStack>
+          <Input placeholder="보호소 상세 주소를 입력해주세요" />
+        </FormControl>
+
+        <FormControl mb={5} isRequired>
+          <FormLabel fontWeight={400}>보호소 전화번호</FormLabel>
+          <Input type="tel" placeholder="전화번호를 입력하세요" />
+        </FormControl>
+
+        <FormControl mb={5} isRequired>
+          <FormLabel fontWeight={400}>보호소 임시 전화번호</FormLabel>
+          <Input type="tel" placeholder="전화번호를 입력하세요" />
+        </FormControl>
+        <Center>
+          <Button
+            type="submit"
+            w="100%"
+            h="44px"
+            pos="absolute"
+            bottom={21}
+            bgColor="orange.400"
+            color="white"
+          >
+            수정완료
+          </Button>
+        </Center>
+      </form>
+    </Box>
+  );
 }
--- apps/volunteer/src/pages/settings/account/index.tsx
+++ apps/volunteer/src/pages/settings/account/index.tsx
@@ -1,3 +1,99 @@
+import {
+  Avatar,
+  Box,
+  Button,
+  Center,
+  FormControl,
+  FormLabel,
+  HStack,
+  Input,
+  Radio,
+  RadioGroup,
+} from '@chakra-ui/react';
+import { useState } from 'react';
+
 export default function SettingsAccountPage() {
-  return <h1>SettingsAccountPage</h1>;
+  const [imgFile, setImgFile] = useState<string>('');
+
+  const uploadImgFile = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      const newImgFile = URL.createObjectURL(file);
+      setImgFile(newImgFile);
+    }
+  };
+  return (
+    <Box px={4} pb={208}>
+      <form>
+        <Center py={30}>
+          <Avatar
+            boxSize={100}
+            as="label"
+            htmlFor="profileImg"
+            cursor="pointer"
+            src={imgFile}
+            variant="프로필 이미지"
+          />
+          <Input
+            id="profileImg"
+            type="file"
+            accept="image/*"
+            onChange={uploadImgFile}
+            display="none"
+          />
+        </Center>
+        <FormControl mb={5}>
+          <FormLabel fontWeight={400}>이메일</FormLabel>
+          <Input
+            disabled
+            bgColor="gray.100"
+            color="gray.500"
+            _hover={{ border: `none` }}
+          />
+        </FormControl>
+        <FormControl mb={5} isRequired>
+          <FormLabel fontWeight={400}>이름</FormLabel>
+          <Input placeholder="이름을 입력하세요" />
+        </FormControl>
+
+        <FormControl mb={5} isRequired>
+          <FormLabel fontWeight={400}>생년월일</FormLabel>
+          <Input type="date" pr="10px" />
+        </FormControl>
+
+        <FormControl mb={5} isRequired>
+          <FormLabel fontWeight={400}>전화번호</FormLabel>
+          <Input type="tel" placeholder="전화번호를 입력하세요" />
+        </FormControl>
+
+        <FormControl mb={5} isRequired>
+          <FormLabel fontWeight={400}>성별</FormLabel>
+          <RadioGroup>
+            <HStack spacing={10}>
+              <Radio colorScheme="orange" value="Male">
+                남성
+              </Radio>
+              <Radio colorScheme="orange" value="Female">
+                여성
+              </Radio>
+            </HStack>
+          </RadioGroup>
+        </FormControl>
+
+        <Center>
+          <Button
+            type="submit"
+            w="100%"
+            h="44px"
+            bgColor="orange.400"
+            color="white"
+            pos="absolute"
+            bottom={21}
+          >
+            수정 완료
+          </Button>
+        </Center>
+      </form>
+    </Box>
+  );
 }
