95a47f0f2926e15178e467272ac3f246bf1e44a6
sukvvon <sukvvon@gmail.com>
2023-11-20 17:53:25 +0900

    feat(shelter, volunteer): 비밀번호 변경 페이지 UI 및 로직 추가

--- apps/shelter/src/pages/settings/password/index.tsx
+++ apps/shelter/src/pages/settings/password/index.tsx
@@ -1,3 +1,142 @@
+import {
+  Box,
+  Button,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Icon,
+  Input,
+  InputGroup,
+  InputRightElement,
+  VStack,
+} from '@chakra-ui/react';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import IoEyeOff from 'shared/assets/IoEyeOff';
+import IoEyeSharp from 'shared/assets/IoEyeSharp';
+import LogoImageBox from 'shared/components/LogoImageBox';
+import useToggle from 'shared/hooks/useToggle';
+import * as z from 'zod';
+
+type Schema = z.infer<typeof schema>;
+
+const schema = z.object({
+  password: z.string().min(1, '기본 비밀번호 정보는 필수입니다'),
+  newPassword: z.string().min(1, '변경 비밀번호 정보는 필수입니다'),
+  // TODO
+  //
+  // .regex(
+  //   /^(?=.*[!@#$%^&*()\-_=+[\]\\|{};:'",<.>/?]+)(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/,
+  //   '비밀번호는 필수 정보입니다(8자 이상)',
+  // ),
+  newPasswordConfirm: z.string().min(1, '변경 비밀번호 확인 정보는 필수입니다'),
+});
+
 export default function SettingsPasswordPage() {
-  return <h1>SettingsPasswordPage</h1>;
+  const [isPasswordShow, togglePasswordShow] = useToggle();
+  const [isNewPasswordShow, toggleNewPasswordShow] = useToggle();
+  const [isNewPasswordConfirmShow, toggleNewPasswordConfirmShow] = useToggle();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Schema>({
+    resolver: zodResolver(schema),
+  });
+
+  const onSubmit = (data: Schema) => {
+    console.log(data);
+  };
+
+  return (
+    <Box px={4} pb="56px">
+      <LogoImageBox />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <FormControl
+          mb={5}
+          isRequired
+          isInvalid={errors.password ? true : false}
+        >
+          <FormLabel>기존 비밀번호</FormLabel>
+          <InputGroup>
+            <Input
+              {...register('password')}
+              placeholder="기존 비밀번호를 입력하세요"
+              type={isPasswordShow ? 'text' : 'password'}
+            />
+            <InputRightElement onClick={togglePasswordShow}>
+              <Icon as={isPasswordShow ? IoEyeOff : IoEyeSharp} />
+            </InputRightElement>
+          </InputGroup>
+          <FormErrorMessage>
+            {errors.password && errors.password.message}
+          </FormErrorMessage>
+        </FormControl>
+        <FormControl
+          mb={5}
+          isRequired
+          isInvalid={errors.newPassword ? true : false}
+        >
+          <FormLabel>변경 비밀번호</FormLabel>
+          <InputGroup>
+            <Input
+              {...register('newPassword')}
+              placeholder="변경 비밀번호를 입력하세요"
+              type={isNewPasswordShow ? 'text' : 'password'}
+            />
+            <InputRightElement onClick={toggleNewPasswordShow}>
+              <Icon as={isNewPasswordShow ? IoEyeOff : IoEyeSharp} />
+            </InputRightElement>
+          </InputGroup>
+          <FormErrorMessage>
+            {errors.newPassword && errors.newPassword.message}
+          </FormErrorMessage>
+        </FormControl>
+        <FormControl
+          mb={10}
+          isRequired
+          isInvalid={errors.newPasswordConfirm ? true : false}
+        >
+          <FormLabel>변경 비밀번호 확인</FormLabel>
+          <InputGroup>
+            <Input
+              {...register('newPasswordConfirm')}
+              placeholder="변경 비밀번호를 다시 입력하세요"
+              type={isNewPasswordConfirmShow ? 'text' : 'password'}
+            />
+            <InputRightElement onClick={toggleNewPasswordConfirmShow}>
+              <Icon as={isNewPasswordConfirmShow ? IoEyeOff : IoEyeSharp} />
+            </InputRightElement>
+          </InputGroup>
+          <FormErrorMessage>
+            {errors.password && errors.password.message}
+          </FormErrorMessage>
+        </FormControl>
+        <VStack
+          maxW="container.sm"
+          mx="auto"
+          px={4}
+          bgColor="white"
+          pos="fixed"
+          bottom={0}
+          left={0}
+          right={0}
+          py={2}
+          zIndex={10}
+          spacing={2}
+          align="stretch"
+        >
+          <Button
+            type="submit"
+            bgColor="orange.400"
+            color="white"
+            _hover={{ bg: undefined }}
+            _active={{ bg: undefined }}
+          >
+            비밀번호 변경하기
+          </Button>
+        </VStack>
+      </form>
+    </Box>
+  );
 }
