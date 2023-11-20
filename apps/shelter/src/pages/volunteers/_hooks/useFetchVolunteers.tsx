4e69c74f997d81f67b17376d591f7bd61716eda4
kutta97 <yhjin04170@gmail.com>
2023-11-21 01:47:28 +0900

    feat(shelter): useFetchVolunteers hook 에 filter params 추가

--- apps/shelter/src/pages/volunteers/_hooks/useFetchVolunteers.tsx
+++ apps/shelter/src/pages/volunteers/_hooks/useFetchVolunteers.tsx
@@ -1,12 +1,30 @@
-import { useSuspenseInfiniteQuery } from '@tanstack/react-query';
+import {
+  InfiniteData,
+  useSuspenseInfiniteQuery,
+  UseSuspenseInfiniteQueryResult,
+} from '@tanstack/react-query';
+import { AxiosResponse } from 'axios';
 
 import { getShelterRecruitments } from '@/apis/recruitment';
+import {
+  RecruitementsResponse,
+  RecruitmentSearchFilter,
+} from '@/types/apis/recruitment';
 
-export default function useFetchVolunteers(pageSize: number) {
+export default function useFetchVolunteers(
+  pageSize = 10,
+  searchFilter?: Partial<RecruitmentSearchFilter>,
+): UseSuspenseInfiniteQueryResult<
+  InfiniteData<AxiosResponse<RecruitementsResponse, Error>>
+> {
   return useSuspenseInfiniteQuery({
-    queryKey: ['recruitments'],
+    queryKey: ['recruitments', searchFilter],
     queryFn: ({ pageParam }) =>
-      getShelterRecruitments({ pageNumber: pageParam, pageSize }),
+      getShelterRecruitments({
+        ...searchFilter,
+        pageNumber: pageParam,
+        pageSize: pageSize,
+      }),
     initialPageParam: 0,
     getNextPageParam: ({ data: { pageInfo } }, _, lastPageParam) =>
       pageInfo.hasNext ? lastPageParam + 1 : null,
