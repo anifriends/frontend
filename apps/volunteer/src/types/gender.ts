0ee43178eff19fe0fc226e7dbf48376fd026fe8d
Choi Won Suk <sukvvon@gmail.com>
2023-11-21 23:52:34 +0900

    feat: 보호소앱 봉사 신청 현황 페이지 UI 추가 및 gender, applicant 관련 type 공통화, 정리 (#156)
    * rename(volunteer): gender 관련 types, constants를 shared로 이동 및 연관 경로 수정
    
    * feat(shelter): recruitment의 applicant 상태 관련 상수 추가
    
    * feat(shelter): recruitment의 applicant 상태 관련 타입 추가
    
    * fix(shelter): recruitment의 applicant 상태, 성별 관련 타입 교체
    
    * feat(shelter): CkCkeck svg 아이콘 추가
    
    * feat(shared): date util에 getAge 함수 추가
    
    * feat(shelter): ManageApplyItem 컴포넌트 추가
    
    * feat(shelter): ApplyInfoItem 컴포넌트 추가
    
    * feat(shelter): ApprovedCountBox 컴포넌트 추가
    
    * feat(shelter): ManageApplyPage 컴포넌트 UI 추가
    
    * feat(shelter): getAge 함수 return 내용 변경

--- apps/volunteer/src/types/gender.ts
+++
@@ -1,7 +0,0 @@
-import { PERAON_GENDER_ENG, PERAON_GENDER_KOR } from '@/constants/gender';
-
-export type PersonGenderEng =
-  (typeof PERAON_GENDER_ENG)[keyof typeof PERAON_GENDER_ENG];
-
-export type PersonGenderKor =
-  (typeof PERAON_GENDER_KOR)[keyof typeof PERAON_GENDER_KOR];
