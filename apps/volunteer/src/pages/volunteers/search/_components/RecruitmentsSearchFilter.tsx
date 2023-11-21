04b99aae200ed28062e97c9af33d2b37c6b2c1d3
kutta97 <yhjin04170@gmail.com>
2023-11-22 02:04:21 +0900

    feat(volunteer): 봉사자앱의 검색 필터 컴포넌트 추가

---
+++ apps/volunteer/src/pages/volunteers/search/_components/RecruitmentsSearchFilter.tsx
@@ -0,0 +1,49 @@
+import { ChangeEvent } from 'react';
+import SearchFilters, {
+  SearchFilterSelectData,
+} from 'shared/components/SearchFilters';
+
+import {
+  PERIOD,
+  RECRUITMENT_STATUS,
+  SEARCH_TYPE,
+} from '@/pages/volunteers/search/_constants/filter';
+import { SearchFilter } from '@/pages/volunteers/search/_types/filter';
+
+type RecruitmentsSearchFilterProps = {
+  searchFilter: Partial<SearchFilter>;
+  onChangeFilter: (event: ChangeEvent<HTMLSelectElement>) => void;
+};
+
+export default function RecruitmentsSearchFilter({
+  searchFilter,
+  onChangeFilter,
+}: RecruitmentsSearchFilterProps) {
+  const searchFilters: SearchFilterSelectData[] = [
+    {
+      selectOption: PERIOD,
+      name: 'period',
+      placeholder: '봉사일',
+      value: searchFilter.period,
+    },
+    {
+      selectOption: RECRUITMENT_STATUS,
+      name: 'recruitmentStatus',
+      placeholder: '모집',
+      value: searchFilter.recruitmentStatus,
+    },
+    {
+      selectOption: SEARCH_TYPE,
+      name: 'searchType',
+      placeholder: '전체',
+      value: searchFilter.searchType,
+    },
+  ];
+
+  return (
+    <SearchFilters
+      searchFilters={searchFilters}
+      onChangeFilter={onChangeFilter}
+    />
+  );
+}
