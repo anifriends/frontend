15afafd8c63318bed0c7d2e5a3e91ff41cecce53
kutta97 <yhjin04170@gmail.com>
2023-11-22 03:00:00 +0900

    refactor(volunteer): 봉사모집 API 함수에 response, request  타입 적용

--- apps/volunteer/src/apis/recruitment.ts
+++ apps/volunteer/src/apis/recruitment.ts
@@ -1,46 +1,15 @@
 import axiosInstance from 'shared/apis/axiosInstance';
 
+import {
+  RecruitementsResponse,
+  RecruitmentsRequest,
+} from '@/types/apis/recruitment';
+
 export const ApplyRecruitments = (recruitmentId: string) =>
   axiosInstance.post(`/recruitments/${recruitmentId}/apply`);
 
-type PageInfo = {
-  totalElements: number;
-  hasNext: boolean;
-};
-
-type Recruitment = {
-  recruitmentId: number;
-  recruitmentTitle: string;
-  recruitmentStartTime: string;
-  recruitmentEndTime: string;
-  recruitmentIsClosed: boolean;
-  recruitmentApplicantCount: number;
-  recruitmentCapacity: number;
-  shelterName: string;
-  shelterImageUrl: string;
-};
-
-type RecruitSearchParams = {
-  keyword: string;
-  startDate: string;
-  endDate: string;
-  isClosed: boolean;
-  content: boolean;
-  title: boolean;
-  shelterName: boolean;
-  pageNumber: number;
-  pageSize: number;
-};
-
-export const getRecruitments = (
-  recruitSearchParams: Partial<RecruitSearchParams>,
-) =>
-  axiosInstance.get<
-    {
-      pageInfo: PageInfo;
-      recruitments: Recruitment[];
-    },
-    RecruitSearchParams
-  >('/recruitments', {
-    params: recruitSearchParams,
-  });
+export const getRecruitments = (request: Partial<RecruitmentsRequest>) =>
+  axiosInstance.get<RecruitementsResponse, RecruitmentsRequest>(
+    '/recruitments',
+    { params: request },
+  );
