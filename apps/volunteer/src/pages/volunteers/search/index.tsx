6124ab46dc7221c200123bd7e6b2769a12a87580
kutta97 <yhjin04170@gmail.com>
2023-11-22 02:07:20 +0900

    feat(volunteer): 봉사자앱의 봉사 모집 검색 페이지 로직 추가

--- apps/volunteer/src/pages/volunteers/search/index.tsx
+++ apps/volunteer/src/pages/volunteers/search/index.tsx
@@ -1,21 +1,63 @@
-import { useEffect } from 'react';
-import useSearchHeaderStore from 'shared/store/searchHeaderStore';
+import { Box } from '@chakra-ui/react';
+import { MouseEvent } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-const handleSearchkeyword = (keyword: string) => {
-  // TODO: VolunteerList 검색 API 호출
-  console.log('[Search Volunteer] keyword:', keyword);
+import VolunteerRecruitItem from '@/pages/volunteers/_components/VolunteerRecruitItem';
+import RecruitmentsSearchFilter from '@/pages/volunteers/search/_components/RecruitmentsSearchFilter';
+import { useRecruitmentSearch } from '@/pages/volunteers/search/_hooks/useRecruitmentSearch';
+
+const DUMMY_RECRUITMENT = {
+  id: 1,
+  title: '봉사자 모집합니다!!',
+  shelterName: '양천구 보건소',
+  shelterProfileImage: 'https://source.unsplash.com/random',
+  volunteerDate: '23.10.23',
+  volunteerDateDday: 12,
+  applicantCount: 2,
+  recruitmentCapacity: 6,
 };
 
+const DUMMY_RECRUITMENT_LIST = Array.from(
+  { length: 4 },
+  () => DUMMY_RECRUITMENT,
+);
+
 export default function VolunteersSearchPage() {
-  const setOnSearch = useSearchHeaderStore((state) => state.setOnSearch);
+  const { isKeywordSearched, searchFilter, handleChangeSearchFilter } =
+    useRecruitmentSearch();
 
-  useEffect(() => {
-    setOnSearch(handleSearchkeyword);
+  const navigate = useNavigate();
 
-    return () => {
-      setOnSearch(() => {});
-    };
-  }, [setOnSearch]);
+  const goVolunteersDetail = (event: MouseEvent<HTMLElement>) => {
+    const recruitmentId = event.currentTarget.getAttribute('data-id');
 
-  return <h1>VolunteersSearchPage</h1>;
+    if (recruitmentId) {
+      navigate(`/volunteers/${recruitmentId}`);
+    }
+  };
+
+  //TODO 삭제 버튼 눌렀을 때 기능 추가
+
+  //TODO recruit id 받아서 마감
+  const closeRecruit = () => {};
+
+  if (!isKeywordSearched) {
+    return null;
+  }
+
+  return (
+    <Box>
+      <RecruitmentsSearchFilter
+        searchFilter={searchFilter}
+        onChangeFilter={handleChangeSearchFilter}
+      />
+      {DUMMY_RECRUITMENT_LIST?.map((recruitment) => (
+        <VolunteerRecruitItem
+          key={recruitment.id}
+          recruitment={recruitment}
+          onClickItem={goVolunteersDetail}
+        />
+      ))}
+    </Box>
+  );
 }
