517bd3ddbd7ecd64f71bbf3c6a301ad15921fc1d
sukvvon <sukvvon@gmail.com>
2023-11-22 00:55:39 +0900

    fix(shared): 특정 글자에만 적용되는 unicode-range 제거

--- packages/shared/fonts/index.tsx
+++ packages/shared/fonts/index.tsx
@@ -10,7 +10,6 @@ export default function Fonts() {
         font-weight: 700;
         font-display: swap;
         src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_20-07@1.0/IBMPlexSansKR-Regular.woff') format('woff');
-        unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
       }
 
       @font-face {
@@ -19,7 +18,6 @@ export default function Fonts() {
         font-weight: 400;
         font-display: swap;
         src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_20-07@1.0/IBMPlexSansKR-Regular.woff') format('woff');
-        unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
       }
     `}
     />
