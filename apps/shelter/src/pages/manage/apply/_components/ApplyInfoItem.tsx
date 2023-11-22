2e5be57fe77f1a5ff894b01a00787b65b70e7f2f
sukvvon <sukvvon@gmail.com>
2023-11-22 16:53:10 +0900

    feat(shelter): ApplyInfoItem에 sticky 속성 추가

--- apps/shelter/src/pages/manage/apply/_components/ApplyInfoItem.tsx
+++ apps/shelter/src/pages/manage/apply/_components/ApplyInfoItem.tsx
@@ -13,13 +13,21 @@ export default function ApplyInfoBox({
   return (
     <Flex
       p={4}
+      pos="sticky"
+      top={0}
       justify="space-between"
       borderBottom="1px solid"
       borderColor="gray.200"
+      bgColor="white"
+      zIndex={10}
     >
       <Text fontSize="sm">
-        총 <Text as="span" fontWeight="semibold">{`${5}명`}</Text>이 봉사를
-        신청했습니다
+        총{' '}
+        <Text
+          as="span"
+          fontWeight="semibold"
+        >{`${currentRecuritmentCount}명`}</Text>
+        이 봉사를 신청했습니다
       </Text>
       <ApplicantStatus
         numerator={currentRecuritmentCount}
