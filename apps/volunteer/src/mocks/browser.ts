f5f20a725bb1d46ef917eb12b8b62591956e400f
Eosdia <lumimomen@gmail.com>
2023-11-22 18:41:29 +0900

    feat(volunteer): get 봉사자정보 msw handler 추가

--- apps/volunteer/src/mocks/browser.ts
+++ apps/volunteer/src/mocks/browser.ts
@@ -1,5 +1,6 @@
 import { setupWorker } from 'msw/browser';
 
 import { handlers } from './handlers/auth';
+import { handlers as volunteerHandlers } from './handlers/volunteer';
 
-export const worker = setupWorker(...handlers);
+export const worker = setupWorker(...handlers, ...volunteerHandlers);
