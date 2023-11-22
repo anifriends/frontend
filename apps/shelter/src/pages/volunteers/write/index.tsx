4c6a9e901dab10c0be4c243791483eb29fdc6d85
DongjaJ <jdh981118@naver.com>
2023-11-22 14:48:42 +0900

    feat(shelter): 게시글 등록 react hook form과 zod 추가

--- apps/shelter/src/pages/volunteers/write/index.tsx
+++ apps/shelter/src/pages/volunteers/write/index.tsx
@@ -10,32 +10,85 @@ import {
   InputRightAddon,
   Textarea,
 } from '@chakra-ui/react';
+import { zodResolver } from '@hookform/resolvers/zod';
 import { useState } from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
 import EditPhotoList from 'shared/components/EditPhotoList';
+import * as z from 'zod';
+
+const recruitmentSchema = z.object({
+  title: z.string().min(1, '제목은 필수로 입력해주세요'),
+  volunteerDate: z.coerce.string(),
+  startTime: z.coerce.string(),
+  endTime: z.coerce.string(),
+  deadLine: z.coerce.string(),
+  capacity: z.coerce.number(),
+  content: z.coerce.string().optional(),
+});
+
+type RecruitmentSchema = z.infer<typeof recruitmentSchema>;
 
 const DUMMY_IMAGE = 'https://source.unsplash.com/random';
 const DUMMY_IMAGE_URLS = Array.from({ length: 2 }, () => DUMMY_IMAGE);
 
 export default function VolunteersWritePage() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<RecruitmentSchema>({
+    resolver: zodResolver(recruitmentSchema),
+  });
   const [imageUrls, setImageUrls] = useState<string[]>(DUMMY_IMAGE_URLS);
+
+  const onSubmit: SubmitHandler<RecruitmentSchema> = (
+    data: RecruitmentSchema,
+  ) => {
+    // alert(JSON.stringify(data));
+    console.log('data', data);
+  };
+
+  // const onInvalid = (error) => console.log(error);
 
   return (
     <Box pt={6} px={4}>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <FormControl mb={5} isRequired>
           <FormLabel>제목</FormLabel>
-          <Input placeholder="모집글 제목을 입력해 주세요" />
+          <Input
+            {...register('title')}
+            placeholder="모집글 제목을 입력해 주세요"
+          />
         </FormControl>
         <FormControl mb={5} isRequired>
-          <FormLabel>봉사일시</FormLabel>
+          <FormLabel>봉사시작 시간</FormLabel>
           <Input
-            placeholder="봉사 일시를 입력해 주세요"
-            type="datetime-local"
+            {...register('volunteerDate')}
+            placeholder="봉사일을 입력해 주세요"
+            type="date"
+          />
+        </FormControl>
+        <FormControl mb={5} isRequired>
+          <FormLabel>봉사시작 일시</FormLabel>
+          <Input
+            {...register('startTime')}
+            placeholder="봉사 시작 시간을 입력해 주세요"
+            type="time"
+          />
+        </FormControl>
+        <FormControl mb={5} isRequired>
+          <FormLabel>봉사종료 일시</FormLabel>
+          <Input
+            {...register('endTime')}
+            placeholder="봉사 종료 시간을 입력해 주세요"
+            type="time"
           />
         </FormControl>
         <FormControl mb={5} isRequired>
           <FormLabel>모집 마감 일시</FormLabel>
           <Input
+            {...register('deadLine')}
             placeholder="모집 마감 일시를 입력해 주세요"
             type="datetime-local"
           />
@@ -43,13 +96,21 @@ export default function VolunteersWritePage() {
         <FormControl mb={5} isRequired>
           <FormLabel>모집 인원</FormLabel>
           <InputGroup>
-            <Input placeholder="모집 인원을 입력해 주세요" />
+            <Input
+              {...register('capacity')}
+              placeholder="모집 인원을 입력해 주세요"
+              type="number"
+            />
             <InputRightAddon>명</InputRightAddon>
           </InputGroup>
         </FormControl>
         <FormControl mb={5}>
           <FormLabel>모집글 상세</FormLabel>
-          <Textarea placeholder="모집글 상세 내용을 입력해 주세요" mb={2} />
+          <Textarea
+            {...register('content')}
+            placeholder="모집글 상세 내용을 입력해 주세요"
+            mb={2}
+          />
           <Flex justifyContent="end">
             <FormHelperText>글자수 0 / 100</FormHelperText>
           </Flex>
@@ -65,9 +126,10 @@ export default function VolunteersWritePage() {
           _hover={{
             bg: undefined,
           }}
-          _active={{
-            bg: undefined,
-          }}
+          // _active={{
+          //   bg: undefined,
+          // }}
+          type="submit"
         >
           등록
         </Button>
