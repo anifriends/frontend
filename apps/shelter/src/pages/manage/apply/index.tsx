ebc73931d4f4baf48b8134f2fff4d170f4ea7f15
sukvvon <sukvvon@gmail.com>
2023-11-22 16:57:04 +0900

    feat(shelter): ManageApplyPage에 msw(api)를 포함한 로직 추가

--- apps/shelter/src/pages/manage/apply/index.tsx
+++ apps/shelter/src/pages/manage/apply/index.tsx
@@ -1,33 +1,42 @@
-import { ShelterRecruitmentApplicant } from '@/types/apis/recruitment';
+import { Box } from '@chakra-ui/react';
+import { useQuery } from '@tanstack/react-query';
+import { useParams } from 'react-router-dom';
+
+import { getShelterRecruitmentApplicants } from '@/apis/recruitment';
+import { APPLICANT_STATUS_ENG } from '@/constants/recruitment';
 
 import ApplyInfoItem from './_components/ApplyInfoItem';
 import ApprovedCountBox from './_components/ApprovedCountBox';
 import ManageApplyItem from './_components/ManageApplyItem';
 
-const applicant: ShelterRecruitmentApplicant = {
-  applicantId: 10,
-  volunteerId: 1,
-  volunteerName: '김영희',
-  volunteerBirthDate: '1997-11-21',
-  volunteerGender: 'FEMALE',
-  completedVolunteerCount: 3,
-  volunteerTemperature: 33,
-  applicantStatus: 'APPROVED',
-};
-
 export default function ManageApplyPage() {
+  const { id } = useParams<{ id: string }>();
+  const { data } = useQuery({
+    queryKey: ['recruitment', 'manage', 'apply', id],
+    queryFn: () => getShelterRecruitmentApplicants(Number(id)),
+    select: ({ data: { applicants, recruitmentCapacity } }) => {
+      return {
+        applicants,
+        recruitmentCapacity,
+        currentRecruitmentCount: applicants.length,
+        approvedCount: applicants.filter(
+          ({ applicantStatus }) =>
+            applicantStatus === APPLICANT_STATUS_ENG.APPROVED,
+        ).length,
+      };
+    },
+  });
+
   return (
-    <>
-      <ApplyInfoItem currentRecuritmentCount={5} recruitmentCapacity={15} />
-      <ManageApplyItem applicant={applicant} />
-      <ManageApplyItem applicant={applicant} />
-      <ManageApplyItem applicant={applicant} />
-      <ManageApplyItem applicant={applicant} />
-      <ManageApplyItem applicant={applicant} />
-      <ManageApplyItem applicant={applicant} />
-      <ManageApplyItem applicant={applicant} />
-      <ManageApplyItem applicant={applicant} />
-      <ApprovedCountBox approvedCount={4} />
-    </>
+    <Box pb={20}>
+      <ApplyInfoItem
+        currentRecuritmentCount={data?.currentRecruitmentCount ?? 0}
+        recruitmentCapacity={data?.recruitmentCapacity ?? 0}
+      />
+      {data?.applicants.map((applicant) => (
+        <ManageApplyItem key={applicant.applicantId} applicant={applicant} />
+      ))}
+      <ApprovedCountBox approvedCount={data?.approvedCount ?? 0} />
+    </Box>
   );
 }
