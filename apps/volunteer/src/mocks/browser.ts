1cda12a1b21cc76ddb3d3e78a8b3cff438987555
Eosdia <lumimomen@gmail.com>
2023-11-23 16:22:54 +0900

    feat(volunteer): 봉사모집상세조회 msw 연결

--- apps/volunteer/src/mocks/browser.ts
+++ apps/volunteer/src/mocks/browser.ts
@@ -2,5 +2,10 @@ import { setupWorker } from 'msw/browser';
 
 import { handlers as authHandlers } from './handlers/auth';
 import { handlers as recruitmentHandler } from './handlers/recruitment';
+import { handlers as shelterHandler } from './handlers/shelter';
 
-export const worker = setupWorker(...authHandlers, ...recruitmentHandler);
+export const worker = setupWorker(
+  ...authHandlers,
+  ...recruitmentHandler,
+  ...shelterHandler,
+);
