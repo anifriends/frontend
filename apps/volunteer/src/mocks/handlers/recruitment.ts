4d37b5544d5b21ae2ed44bbc97a97e38e8758dde
Eosdia <lumimomen@gmail.com>
2023-11-23 16:21:37 +0900

    feat(volunteer): 봉사모집상세조회 mock api 추가

--- apps/volunteer/src/mocks/handlers/recruitment.ts
+++ apps/volunteer/src/mocks/handlers/recruitment.ts
@@ -32,4 +32,27 @@ export const handlers = [
       { status: 200 },
     );
   }),
+  http.get('/recruitments/:id', async () => {
+    await delay(200);
+    return HttpResponse.json(
+      {
+        recruitmentTitle: '강아지봉사자를 모집합니다.',
+        recruitmentApplicantCount: 5,
+        recruitmentCapacity: 10,
+        recruitmentContent: '가짜 데이터 내용입니다!!! '.repeat(50),
+        recruitmentStartTime: '2023-12-17T14:00:00',
+        recruitmentEndTime: '2023-12-17T16:00:00',
+        recruitmentIsClosed: false,
+        recruitmentDeadline: '2023-12-18T18:00:00',
+        recruitmentCreatedAt: '2023-12-15T14:00:00',
+        recruitmentUpdatedAt: '2023-12-15T14:00:00',
+        recruitmentImageUrls: [
+          'https://source.unsplash.com/random/?person',
+          'https://source.unsplash.com/random/300X500',
+        ],
+        shelterId: 3,
+      },
+      { status: 200 },
+    );
+  }),
 ];
