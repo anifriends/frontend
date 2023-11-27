cacde57fb89ec051c6542c02677f3ee944361ba0
woo hyeonji <117665863+Eosdia@users.noreply.github.com>
2023-11-27 15:02:47 +0900

    feat: 계정정보수정 (봉사자, 보호소) msw 연결 (#167)
    * feat(shelter): 계정정보수정 msw mock 데이터
    
    * fix(shelter): 보호소 정보 msw handler shelterImageUrl 수정
    
    * feat(shelter): shelterInfo 수정 타입 추가
    
    * fix(shelter): 계정정보수정API request 타입 수정
    
    * feat(shelter): 보호소계정정보 useQuery 추가
    
    * feat(shelter): 보호소 계정정보 조회 msw 연결
    
    * feat(shelter): 계정정보 수정 mutation 추가
    
    * fix(shelter): msw 계정정보수정 handler HttpResponse 수정
    
    * design(shelter): button 의 hover, active background 수정
    
    * fix(volunteer): api MyInfoResponse, UpdateUserInfoParams 타입 수정
    
    * feat(volunteer): 봉사자계정정보 fetch hook 추가
    
    * feat(volunteer): get 봉사자정보 msw handler 추가
    
    * feat(volunteer): 봉사자 계정정보 조회 msw 연결
    
    * design(volunteer): button 태그 hover, active 일때 background 제거
    
    * fix(volunteer): volunteergender 타입 수정
    
    * feat(volunteer): 계정정보수정 useMutation 추가
    
    * feat(volunteer): 계정정보수정 patch msw 추가
    
    * fix(volunteer): 계정정보조회 useQuery 의 select 메소드 타입 지정

--- apps/volunteer/src/mocks/browser.ts
+++ apps/volunteer/src/mocks/browser.ts
@@ -2,5 +2,6 @@ import { setupWorker } from 'msw/browser';
 
 import { handlers as authHandlers } from './handlers/auth';
 import { handlers as recruitmentHandler } from './handlers/recruitment';
+import { handlers as volunteerHandlers } from './handlers/volunteer';
 
-export const worker = setupWorker(...authHandlers, ...recruitmentHandler);
+export const worker = setupWorker(...authHandlers, ...recruitmentHandler, ...volunteerHandlers );
