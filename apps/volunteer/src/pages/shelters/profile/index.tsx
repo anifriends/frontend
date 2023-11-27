53ac53959ff1739d5866e8660cd5d9d213313b2e
Eosdia <lumimomen@gmail.com>
2023-11-28 02:37:05 +0900

    feat(volunteer): 보호소 프로필 데이터 msw 연결

--- apps/volunteer/src/pages/shelters/profile/index.tsx
+++ apps/volunteer/src/pages/shelters/profile/index.tsx
@@ -1,24 +1,43 @@
 import { Box, Divider } from '@chakra-ui/react';
+import { useQuery } from '@tanstack/react-query';
+import { useParams } from 'react-router-dom';
 import InfoTextList from 'shared/components/InfoTextList';
 import ProfileInfo from 'shared/components/ProfileInfo';
 import Tabs from 'shared/components/Tabs';
+
+import { getShelterProfileDetail } from '@/apis/shelter';
 
 import ShelterRecruitments from './_components/ShelterRecruitments';
 import ShelterReviews from './_components/ShelterReviews';
 
 export default function SheltersProfilePage() {
+  const { id } = useParams();
+  const shelterId = Number(id);
+
+  const { data } = useQuery({
+    queryKey: ['shelter', 'profile', shelterId],
+    queryFn: async () => (await getShelterProfileDetail(shelterId)).data,
+  });
+
   return (
     <Box>
       <ProfileInfo
-        infoTitle="양천구 보호소"
-        infoTexts={['shelter@gmail.com', '비공개']}
+        infoImage={data?.shelterImageUrl}
+        infoTitle={data?.shelterName ?? ''}
+        infoTexts={[data?.shelterEmail ?? '', data?.shelterAddress ?? '']}
       />
       <Divider />
       <InfoTextList
         infoTextItems={[
-          { title: '전화번호', content: '010-1234-5678' },
-          { title: '전화번호(임시)', content: '02-123-4567' },
-          { title: '상세주소', content: '비공개' },
+          { title: '전화번호', content: data?.shelterPhoneNumber ?? '' },
+          {
+            title: '전화번호(임시)',
+            content: data?.shelterSparePhoneNumber ?? '',
+          },
+          {
+            title: '상세주소',
+            content: data?.shelterAddressDetail ?? '비공개',
+          },
         ]}
       />
       <Tabs
