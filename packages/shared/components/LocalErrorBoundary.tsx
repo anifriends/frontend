1eabb841fa27ff3567f22f92933e77d73c7ebb69
DongjaJ <jdh981118@naver.com>
2023-11-28 14:41:54 +0900

    feat(shared): response가 없는 경우 고려해서error boundary 수정

--- packages/shared/components/LocalErrorBoundary.tsx
+++ packages/shared/components/LocalErrorBoundary.tsx
@@ -1,12 +1,14 @@
-import { Button, Heading, Text, VStack } from '@chakra-ui/react';
+import { Button, Flex, Heading, Text, VStack } from '@chakra-ui/react';
 import { ErrorBoundary, FallbackProps } from 'react-error-boundary';
 import { useNavigate } from 'react-router-dom';
-import { getErrorMessage } from 'utils/errorMessage';
+
+import { getErrorMessage } from '../utils/errorMessage';
 
 const RetryErrorFallback = ({ error, resetErrorBoundary }: FallbackProps) => {
-  const { status } = error.response;
-  const { title, content } = getErrorMessage(status);
   const navigate = useNavigate();
+  const status = error?.response?.status;
+  const { title, content } = getErrorMessage(status);
+
   const isNotAuthorized = status === 401;
   const isForbidden = status === 403;
   const buttonMessage = isNotAuthorized ? '로그인' : '다시 시도';
@@ -25,15 +27,17 @@ const RetryErrorFallback = ({ error, resetErrorBoundary }: FallbackProps) => {
         {title}
       </Heading>
       <Text color="gray.400">{content}</Text>
-      {!isForbidden && <Button onClick={onClick}>{buttonMessage}</Button>}
-      <Button onClick={() => navigate('/volunteers')}>홈으로</Button>
+      <Flex>
+        {!isForbidden && <Button onClick={onClick}>{buttonMessage}</Button>}
+        <Button onClick={() => navigate('/volunteers')}>홈으로</Button>
+      </Flex>
     </VStack>
   );
 };
 
 const FallbackComponent = (props: FallbackProps) => {
   // TODO accessToken 갱신 api가 정상화되면
-  //만약 api error혹은 401에러가 발생하면 throw props.error
+  //만약 401, 403, 404 혹은 그 외 에러가 발생하면 throw props.error
 
   return <RetryErrorFallback {...props} />;
 };
--- packages/shared/layout/index.tsx
+++ packages/shared/layout/index.tsx
@@ -1,7 +1,8 @@
 import { Box, Container } from '@chakra-ui/react';
-import { useLayoutEffect } from 'react';
+import { useEffect } from 'react';
 import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 
+import LocalErrorBoundary from '../components/LocalErrorBoundary';
 import useAccessTokenMutation from '../hooks/useAccessTokenMutation';
 import { AppType } from '../types/app';
 import BottomNavBar from './BottomNavBar';
@@ -16,23 +17,33 @@ export default function Layout({ appType }: LayoutProps) {
   const { pathname } = useLocation();
   const { mutate, isPending } = useAccessTokenMutation();
 
-  useLayoutEffect(() => {
-    mutate(undefined, {
-      onSuccess: () => {
-        if (pathname === '/') {
-          navigate('/volunteers');
-        }
-      },
-      onError: (error) => {
-        console.warn(error);
-        if (appType === 'SHELTER_APP') {
-          navigate('/signin');
-        }
-        if (appType === 'VOLUNTEER_APP' && pathname === '/') {
-          navigate('/volunteers');
-        }
-      },
-    });
+  useEffect(() => {
+    if (pathname === '/') {
+      if (appType === 'VOLUNTEER_APP') {
+        navigate('/volunteers');
+      } else {
+        navigate('/signin');
+      }
+    }
+
+    //TODO 액세스 토큰 갱신 api 정상화 되면 연결
+
+    // mutate(undefined, {
+    //   onSuccess: () => {
+    //     if (pathname === '/') {
+    //       navigate('/volunteers');
+    //     }
+    //   },
+    //   onError: (error) => {
+    //     console.warn(error);
+    //     if (appType === 'SHELTER_APP') {
+    //       navigate('/signin');
+    //     }
+    //     if (appType === 'VOLUNTEER_APP' && pathname === '/') {
+    //       navigate('/volunteers');
+    //     }
+    //   },
+    // });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
@@ -44,7 +55,9 @@ export default function Layout({ appType }: LayoutProps) {
     <Container pos="relative" maxW="container.sm" h="100vh" p={0} centerContent>
       <Header appType={appType} />
       <Box overflowY="scroll" width="100%" height="100%" as="main">
-        <Outlet />
+        <LocalErrorBoundary>
+          <Outlet />
+        </LocalErrorBoundary>
       </Box>
       <BottomNavBar />
     </Container>
