1122b230c4cd536a27d02f55616f554825261826
kutta97 <yhjin04170@gmail.com>
2023-11-28 21:37:22 +0900

    refactor(volunteer): 봉사 후기 글자수 제한을 상수로 분리

--- apps/volunteer/src/pages/shelters/reviews/_schema/reviewSchema.ts
+++ apps/volunteer/src/pages/shelters/reviews/_schema/reviewSchema.ts
@@ -1,10 +1,15 @@
 import { z } from 'zod';
 
+import { MAX_REVIEW_CONTENTS_LENGTH } from '@/pages/shelters/reviews/_constants/reviews';
+
 export const reviewSchema = z.object({
   content: z
     .string()
     .optional()
-    .refine((val) => val?.length && val.length < 500, '에러입니다'),
+    .refine(
+      (val) => val?.length && val.length < MAX_REVIEW_CONTENTS_LENGTH,
+      '에러입니다',
+    ),
 });
 
 export type ReviewSchema = z.infer<typeof reviewSchema>;
