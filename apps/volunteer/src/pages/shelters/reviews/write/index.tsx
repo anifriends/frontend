1cb8c871f83dfeec7106d7a687bb6d17052f5ba3
Hyejin Yang <yhjin04170@gmail.com>
2023-11-28 14:38:04 +0900

    feat(volunteer): 봉사 후기 작성 및 수정 페이지 라우팅 수정 (#196)
    * feat(volunteer): 리뷰 작성 및 수정 페이지 경로 수정
    
    * feat(volunteer): 리뷰 작성 및 수정 페이지에서 useParams 를 통해 아이디 가져오는 기능 추가

--- apps/volunteer/src/pages/shelters/reviews/write/index.tsx
+++ apps/volunteer/src/pages/shelters/reviews/write/index.tsx
@@ -1,3 +1,8 @@
+import { useParams } from 'react-router-dom';
+
 export default function SheltersReviewsWritePage() {
+  const { shelterId, applicantId } = useParams();
+  console.log(shelterId, applicantId);
+
   return <h1>SheltersReviewsWritePage</h1>;
 }
