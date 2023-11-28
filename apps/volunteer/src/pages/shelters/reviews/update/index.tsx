2f7bbbfd1596ce326a85370be8c359bf85a9dbe1
kutta97 <yhjin04170@gmail.com>
2023-11-28 14:33:20 +0900

    feat(volunteer): 리뷰 작성 및 수정 페이지에서 useParams 를 통해 아이디 가져오는 기능 추가

--- apps/volunteer/src/pages/shelters/reviews/update/index.tsx
+++ apps/volunteer/src/pages/shelters/reviews/update/index.tsx
@@ -1,3 +1,8 @@
+import { useParams } from 'react-router-dom';
+
 export default function SheltersReviewsUpdatePage() {
+  const { shelterId, reviewId } = useParams();
+  console.log(shelterId, reviewId);
+
   return <h1>SheltersReviewsUpdatePage</h1>;
 }
