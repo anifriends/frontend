ac022087187bdbcb3c7145b105c34343833c9377
Eosdia <lumimomen@gmail.com>
2023-11-29 01:56:19 +0900

    feat(volunteer): 보호소의 봉사모집리스트 조회 api 추가

--- apps/volunteer/src/apis/shelter.ts
+++ apps/volunteer/src/apis/shelter.ts
@@ -19,3 +19,42 @@ export const getShelterProfileDetail = (shelterId: number) =>
     shelterPhoneNumber: string;
     shelterSparePhoneNumber: string;
   }>(`/shelters/${shelterId}/profile`);
+
+export type RecruitmentOfShleter = {
+  recruitmentId: number;
+  recruitmentTitle: string;
+  recruitmentStartTime: string;
+  recruitmentDeadline: string;
+  recruitmentApplicantCount: number;
+  recruitmentCapacity: number;
+};
+
+type PageInfo = {
+  totalElements: number;
+  hasNext: boolean;
+};
+
+type RecruitmentsOfShelterResponse = {
+  pageInfo: PageInfo;
+  recruitments: RecruitmentOfShleter[];
+};
+
+type RecruitmentOfShelterRequest = {
+  page: number;
+  size: number;
+};
+
+export const getRecruitementsOfShelter = (
+  shelterId: number,
+  page: number,
+  size: number,
+) =>
+  axiosInstance.get<RecruitmentsOfShelterResponse, RecruitmentOfShelterRequest>(
+    `/shelters/${shelterId}/recruitments`,
+    {
+      params: {
+        page,
+        size,
+      },
+    },
+  );
