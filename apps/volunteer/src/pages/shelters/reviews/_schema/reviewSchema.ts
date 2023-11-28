f5c84998a6670a05ec0a2b1b1ab1506489b4b5cb
kutta97 <yhjin04170@gmail.com>
2023-11-28 22:31:43 +0900

    feat(volunteer): reviewSchema 에서 contents 에 존재하던 optional 속성 제거

--- apps/volunteer/src/pages/shelters/reviews/_schema/reviewSchema.ts
+++ apps/volunteer/src/pages/shelters/reviews/_schema/reviewSchema.ts
@@ -5,7 +5,6 @@ import { MAX_REVIEW_CONTENTS_LENGTH } from '@/pages/shelters/reviews/_constants/
 export const reviewSchema = z.object({
   content: z
     .string()
-    .optional()
     .refine(
       (val) => val?.length && val.length < MAX_REVIEW_CONTENTS_LENGTH,
       '에러입니다',
