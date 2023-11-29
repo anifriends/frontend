edcabfbddc9f966750faf1631fea07b54fac9696
Choi Won Suk <sukvvon@gmail.com>
2023-11-29 16:52:28 +0900

    feat: 보호소앱 봉사 신청 현황 페이지 msw(api), 로직 추가 (#165)
    * feat(shelter): 봉사자 신청 현황 관련 상태 '승인', '거절' 추가
    
    * feat(shelter): 봉사자 신청 현황 관련 msw handlers 추가
    
    * fix(shelter): 봉사자 신청 현황 관련 타입 status에서 isAprroved로 변경
    
    * feat(shelter): CkClose svg 아이콘 추가
    
    * feat(shelter): ApplyInfoItem에 sticky 속성 추가
    
    * fix(shelter): ApprovedCounBox의 bottom 속성 수정
    
    * feat(shelter): ManageApplyItem에 msw(api)를 포함한 로직 추가
    
    * feat(shelter): ManageApplyPage에 msw(api)를 포함한 로직 추가
    
    * fix(shelter): 봉사 신청 현황 관련 handler의 id값 수정
    
    * fix(shelter): MangeApplyItem의 mutation에 volunteerId를 recruitmentId로 변경
    
    * fix(shelter): ManageApplyPage의 query에 volunteerId를 recruitmentId로, suspense로 변경
    
    * docs(shelter): 빌드 테스트를 위한 일시적인 주석 추가

--- apps/shelter/src/pages/manage/apply/index.tsx
+++ apps/shelter/src/pages/manage/apply/index.tsx
@@ -1,33 +1,53 @@
-import { ShelterRecruitmentApplicant } from '@/types/apis/recruitment';
+import { Box } from '@chakra-ui/react';
+import { useSuspenseQuery } from '@tanstack/react-query';
+import { useParams } from 'react-router-dom';
+
+import { getShelterRecruitmentApplicants } from '@/apis/recruitment';
+import { APPLICANT_STATUS_ENG } from '@/constants/recruitment';
 
 import ApplyInfoItem from './_components/ApplyInfoItem';
 import ApprovedCountBox from './_components/ApprovedCountBox';
 import ManageApplyItem from './_components/ManageApplyItem';
 
-const applicant: ShelterRecruitmentApplicant = {
-  applicantId: 10,
-  volunteerId: 1,
-  volunteerName: '김영희',
-  volunteerBirthDate: '1997-11-21',
-  volunteerGender: 'FEMALE',
-  completedVolunteerCount: 3,
-  volunteerTemperature: 33,
-  applicantStatus: 'APPROVED',
-};
-
 export default function ManageApplyPage() {
+  const { id: recruitmentId } = useParams<{ id: string }>();
+  const {
+    data: {
+      currentRecruitmentCount,
+      recruitmentCapacity,
+      applicants,
+      approvedCount,
+    },
+  } = useSuspenseQuery({
+    queryKey: ['recruitment', 'manage', 'apply', Number(recruitmentId)],
+    queryFn: () => getShelterRecruitmentApplicants(Number(recruitmentId)),
+    select: ({ data: { applicants, recruitmentCapacity } }) => {
+      return {
+        applicants,
+        recruitmentCapacity,
+        currentRecruitmentCount: applicants.length,
+        approvedCount: applicants.filter(
+          ({ applicantStatus }) =>
+            applicantStatus === APPLICANT_STATUS_ENG.APPROVED,
+        ).length,
+      };
+    },
+  });
+
   return (
-    <>
-      <ApplyInfoItem currentRecuritmentCount={5} recruitmentCapacity={15} />
-      <ManageApplyItem applicant={applicant} />
-      <ManageApplyItem applicant={applicant} />
-      <ManageApplyItem applicant={applicant} />
-      <ManageApplyItem applicant={applicant} />
-      <ManageApplyItem applicant={applicant} />
-      <ManageApplyItem applicant={applicant} />
-      <ManageApplyItem applicant={applicant} />
-      <ManageApplyItem applicant={applicant} />
-      <ApprovedCountBox approvedCount={4} />
-    </>
+    <Box pb={20}>
+      <ApplyInfoItem
+        currentRecuritmentCount={currentRecruitmentCount}
+        recruitmentCapacity={recruitmentCapacity}
+      />
+      {applicants.map((applicant) => (
+        <ManageApplyItem
+          key={applicant.applicantId}
+          recruitmentId={Number(recruitmentId)}
+          applicant={applicant}
+        />
+      ))}
+      <ApprovedCountBox approvedCount={approvedCount} />
+    </Box>
   );
 }
