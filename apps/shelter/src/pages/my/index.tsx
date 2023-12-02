ea9f45b04fa71597ed9975c7cedcdbfe5ccca7ea
kutta97 <yhjin04170@gmail.com>
2023-12-02 18:20:08 +0900

    fix(shelter): 마이페이지 로그아웃 버튼 중복 제거

--- apps/shelter/src/pages/my/index.tsx
+++ apps/shelter/src/pages/my/index.tsx
@@ -99,7 +99,6 @@ function ShelterMy() {
           settingItems={[
             { itemTitle: '계정 정보 수정하기', onClick: goSettingsAccount },
             { itemTitle: '비밀번호 변경하기', onClick: goSettingsPassword },
-            { itemTitle: '로그아웃하기', onClick: goSettingsPassword },
           ]}
         />
         <SettingGroup
