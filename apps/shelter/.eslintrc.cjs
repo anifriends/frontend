d414d41dfff66793745d9bdd62cd7d4c939f89f0
DongjaJ <jdh981118@naver.com>
2024-01-16 20:18:14 +0900

    chore(shelter): vitest, testing library 등 테스팅 환경 설정

--- apps/shelter/.eslintrc.cjs
+++ apps/shelter/.eslintrc.cjs
@@ -1,4 +1,25 @@
 module.exports = {
   root: true,
-  extends: ["custom"],
+  extends: ['custom'],
+  overrides: [
+    {
+      files: ['__mocks__/**.ts', 'src/**/*.spec.ts', 'src/**/*.spec.tsx'],
+      plugins: ['vitest'],
+      extends: ['plugin:vitest/recommended'],
+      rules: {
+        'vitest/expect-expect': 'off',
+      },
+      globals: {
+        globalThis: true,
+        describe: true,
+        it: true,
+        expect: true,
+        beforeEach: true,
+        afterEach: true,
+        beforeAll: true,
+        afterAll: true,
+        vi: true,
+      },
+    },
+  ],
 };
